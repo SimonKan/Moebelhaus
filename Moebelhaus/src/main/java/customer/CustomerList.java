087d006af8fafdac326eb9a178febbb7c0d89ae8
SimonKan <simany@web.de>
2013-02-05 23:51:40 +0100

    Klasse CustomerList vollständig erstellt

--- Moebelhaus/src/main/java/customer/CustomerList.java
+++ Moebelhaus/src/main/java/customer/CustomerList.java
@@ -1,38 +1,158 @@
 package customer;
 
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class CustomerList {
 
+    private List<Customer> customers;
+// Liste wird erstellt
+
+    public CustomerList() {
+        customers = new ArrayList<Customer>();
+    }
+
     /**
      *
-     * @param Id
+     * neuer Konstruktor zum Übertragen der List<Customer> in eine
+     * {@link ArticleList}.
+     *
+     * @param articles Kunden der Liste
      */
-    public Customer getCustomerById(long Id) {
+    public CustomerList(List<Customer> customer) {
+        if (customer == null) {
+            throw new IllegalArgumentException("customer was null");
+        }
+        this.customers = new ArrayList<Customer>(customer);
+    }
+
+    /**
+     *
+     * @param customer ein neuer Kunde
+     *
+     * @return {@code true} wenn der Kunde neu zur Liste hinzukam, {@code false}
+     * falls der Kunde bereits in der Liste ist
+     */
+    public boolean add(Customer customer) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    /**
+     *
+     * @param customer der Kunde der aus der Liste zu entfernen ist
+     *
+     * @return {@code true} wenn der Kunde zuvor in der Liste war, {@code false}
+     * sonst
+     */
+    public boolean remove(Customer customer) {
         throw new UnsupportedOperationException();
     }
 
     /**
      *
-     * @param lastName
+     * @param customer ein Kunde
+     *
+     * @return {@code true} wenn der Artikel in der Liste ist, {@code false}
+     * sonst
      */
-    public Set<Customer> getCustomersByLastName(String lastName) {
+    public boolean contains(Customer customer) {
         throw new UnsupportedOperationException();
     }
 
-    /**
-     *
-     * @param Id
+    /*
+     * Methoden zum Durchsuchen der Liste nach uniqueId, articleId, name,
+     * material, price Rückgabetyp jeweils eine neue Liste die mit den Elementen
+     * gefüllt wird
      */
-    public Customer deleteCustomerById(long Id) {
-        throw new UnsupportedOperationException();
+    /**
+     * Durchsucht die Kundenliste nach einem Kunden anhand der UniqueId
+     *
+     * @param id
+     * @return {@link Customer customer}
+     */
+    public Customer getCustomerById(long id) {
+        for (Customer customer : customers) {
+            if (customer.getId() == id) {
+                return customer;
+            }
+        }
+        return null;
     }
 
     /**
+     * Durchsucht die CustomerList anhand des FirstName und gibt eine Liste der
+     * Customer zurück
      *
-     * @param customer
+     * @param firstName Name des Customers
+     * @return {@link CustomerList searchlist}
+     * @throws IllegalArgumentException <ul><li>wenn Name null ist</li><li>wenn
+     * name leer ist</li><li>wenn der name nicht dem Schema entspricht</li><ul>
      */
-    public void addCustomer(Customer customer) {
-        throw new UnsupportedOperationException();
+    public CustomerList getCustomerByFirstName(String firstName) {
+        if (firstName == null) {
+            throw new IllegalArgumentException("name was null");
+        }
+        firstName = firstName.trim().toLowerCase();
+        if (firstName.isEmpty()) {
+            throw new IllegalArgumentException("name is empty");
+        }
+        List<Customer> searchList = new ArrayList<Customer>();
+
+
+        return new CustomerList(searchList);
+    }
+
+    /**
+     * Durchsucht die CustomerListe anhand des LastName und gibt eine Liste der
+     * Customer zurück
+     *
+     * @param lastName Name des Customer
+     * @return {@link Customerlist searchlist}
+     * @throws IllegalArgumentException <ul><li>wenn Name null ist</li><li>wenn
+     * name leer ist</li><li>wenn der name nicht dem Schema entspricht</li><ul>
+     */
+    public CustomerList getCustomerByLastName(String lastName) {
+        if (lastName == null) {
+            throw new IllegalArgumentException("lastName was null");
+        }
+        lastName = lastName.trim().toLowerCase();
+        if (lastName.isEmpty()) {
+            throw new IllegalArgumentException("lastName is empty");
+        }
+        List<Customer> searchList = new ArrayList<Customer>();
+
+
+
+        return new CustomerList(searchList);
+    }
+
+    /**
+     * Durchsucht die CustomerListe anhand der Straße und gibt eine Liste der
+     * Customer zurück
+     *
+     * @param street Straße Customer
+     * @return {@link Customerlist searchlist}
+     * @throws IllegalArgumentException <ul><li>wenn Name null ist</li><li>wenn
+     * name leer ist</li><li>wenn der name nicht dem Schema entspricht</li><ul>
+     */
+    public CustomerList getCustomerByStreet(String street) {
+        if (street == null) {
+            throw new IllegalArgumentException("street was null");
+        }
+        street = street.trim().toLowerCase();
+        if (street.isEmpty()) {
+            throw new IllegalArgumentException("street is empty");
+        }
+        List<Customer> searchList = new ArrayList<Customer>();
+
+
+
+        return new CustomerList(searchList);
+    }
+
+    public List<Customer> toList() {
+        return new ArrayList<Customer>(customers);
     }
 }
