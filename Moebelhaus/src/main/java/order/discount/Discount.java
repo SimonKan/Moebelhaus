93aa1682ae122872bd820832368b72fd648e846b
Immanuel Haffner <s9imhaff@stud.uni-saarland.de>
2013-02-20 19:47:08 +0100

    Discount ist nun eine abstrakte Klasse, die eine ID besitzt. Die ID gibt außerdem an, in welcher Reihenfolge Discounts angewandt werden (kleinere ID bedeutet früher anwenden).

--- Moebelhaus/src/main/java/order/discount/Discount.java
+++ Moebelhaus/src/main/java/order/discount/Discount.java
@@ -8,20 +8,51 @@ package order.discount;
 import order.Order;
 
 /**
- * Discount realisiert ein KommandPattern
- * Die Order ruft die Methode apply des Discounts auf, sodass Discount auf die
- * Summe der Order angewand wird
+ * Discount realisiert ein KommandPattern Die Order ruft die Methode apply des
+ * Discounts auf, sodass Discount auf die Summe der Order angewand wird
  *
  * @author Simon
  * @since 04.02.2013
  * @version 1.0.0
  *
  */
-public interface Discount {
+public abstract class Discount implements Comparable<Discount> {
+
+    private final int id;
+
+    public Discount(int id) {
+        this.id = id;
+    }
 
     /**
      * TODO Implementiern
+     *
      * @param order
      */
-    void apply(Order order);
+    public abstract void apply(Order order);
+
+    @Override
+    public int compareTo(Discount other) {
+        return this.id - other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        final Discount other = (Discount) obj;
+        return this.id == other.id;
+    }
 }
